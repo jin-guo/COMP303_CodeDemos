62b58c823b7cbae091c59c5b71abcce70d4fb228
Jin Guo <jin.lc.guo@gmail.com>
2020-01-15 13:58:29 -0500

    Fixed the annotation related to student interface (leave for module 2).

--- src/ca/mcgill/cs/swdesign/m1/EscapingReference/Undergrad.java
+++ src/ca/mcgill/cs/swdesign/m1/EscapingReference/Undergrad.java
@@ -1,6 +1,6 @@
 package ca.mcgill.cs.swdesign.m1.EscapingReference;
 
-public class Undergrad implements Student {
+public class Undergrad {
     private String aID;
     private String aFirstName;
     private String aLastName;
@@ -11,37 +11,30 @@ public class Undergrad implements Student {
         this.aLastName = pLastName;
     }
 
-    @Override
     public String getFirstName() {
         return aFirstName;
     }
 
-    @Override
     public void setFirstName(String pFirstName) {
         this.aFirstName = pFirstName;
     }
 
-    @Override
     public String getLastName() {
         return aLastName;
     }
 
-    @Override
     public void setLastName(String pLastName) {
         this.aLastName = pLastName;
     }
 
-    @Override
     public String getID() {
         return aID;
     }
 
-    @Override
     public void setID(String pID) {
         this.aID = pID;
     }
 
-    @Override
     public String toString() {
         return aID + ", " + aFirstName + ", " + aLastName;
     }
