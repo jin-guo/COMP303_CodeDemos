fc0e4d04e5bd78bc856be9495905326ebf8041e3
Jin Guo <9352437+jin-guo@users.noreply.github.com>
2023-04-03 12:28:36 -0400

    Update iterating list.

--- src/ca/mcgill/cs/swdesign/m9/IteratingList.java
+++ src/ca/mcgill/cs/swdesign/m9/IteratingList.java
@@ -11,8 +11,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class IteratingList
 {
 	
-	private static List<String> strings = new CopyOnWriteArrayList<>();
-//	private static List<String> strings = new ArrayList<>();
+	private static List<String> strings = new CopyOnWriteArrayList<>(); //  Compare the usage with synchronizedList
+//	private static List<String> strings = Collections.synchronizedList(new ArrayList<>());
 
 	public static void main(String[] args) throws InterruptedException
 	{
@@ -25,16 +25,12 @@ public class IteratingList
 			@Override
 			public void run()
 			{
-			synchronized (strings) {
 				Iterator<String> it = strings.iterator();
 				while (it.hasNext()) {
 					String s = it.next();
 					System.out.printf("1st Thread: %s\n", s);
-					if(s.equals("5"))
-						it.remove();
 				}
-
-			}
+				System.out.printf("String size to 1st Thread: %d\n", strings.size());
 
 			}
 
@@ -45,18 +41,19 @@ public class IteratingList
 			@Override
 			public void run()
 			{
-					for(int i = 10;i<2000;i++)
+					for (int i = 10; i < 2000; i++) {
 						strings.add(String.valueOf(i));
+						System.out.printf(".");
+					}
 					System.out.printf("2nd Thread done\n");
-					System.out.printf("String size: %d\n", strings.size());
-
+					System.out.printf("String size to 2nd thread: %d\n", strings.size());
 			}
 
 		});
 
 		tA.start();
 		tB.start();
-
+		System.out.printf("String size to Main: %d\n", strings.size());
 	}
 }
 		    
