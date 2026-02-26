2433fcaa3328a5b5c33994a21a8f4d21fdef9c2f
Jin Guo <9352437+jin-guo@users.noreply.github.com>
2026-02-26 11:31:21 -0500

    Add code demo for M6.

---
+++ src/ca/mcgill/cs/swdesign/m6/KeyboardShortcut.java
@@ -0,0 +1,6 @@
+package ca.mcgill.cs.swdesign.m6;
+
+public interface KeyboardShortcut {
+    void clicked();
+    void undo();
+}
---
+++ src/ca/mcgill/cs/swdesign/m6/Shape.java
@@ -0,0 +1,6 @@
+package ca.mcgill.cs.swdesign.m6;
+
+public interface Shape extends Cloneable {
+    void move(int x, int y);
+    Shape clone();
+}
